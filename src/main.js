9828273d44c8a8d4d18f13f34a1788c6021dc7aa
nenekelvin <karvinmitchar@gmail.com>
2020-10-12 15:12:12 +0300

    routing

--- src/main.js
+++ src/main.js
@@ -3,12 +3,25 @@ import App from './App.vue'
 import VueResource from 'vue-resource'
 import VuePaginate from 'vue-paginate'
 import VueRouter from 'vue-router'
+import PostDetail from './PostDetail.vue'
+import Blog from './Blog.vue'
 
 Vue.use(VueResource);
 Vue.use(VuePaginate);
 Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', component: Blog },
+  { path: '/post/:id', component: PostDetail }
+];
+
+const router = new VueRouter({
+  mode:'history',
+  routes
+})
+
 new Vue({
- el:'#app',
- render:h=>h(App)
+  el:'#app',
+  router,
+  render:h=>h(App)
 })
