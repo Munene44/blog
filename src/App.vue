ab74359215f780ca28a965b8a0382b180c97ce21
nenekelvin <karvinmitchar@gmail.com>
2020-10-12 15:10:34 +0300

    vue router plugin

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <div class="col-sm-12">
         <div class="jumbotron">
           <div class="container">
-            <span class="display-4 h1">Vue.js Blog Tutorial #1</span>
-            <p class="lead">Simple example how to connect to the server, paginate results, create single page app...</p>
+            <span class="display-4 h1">Blog </span>
+            <p class="lead">Simple example how to connect to the server, paginate results, create single page app</p>
           </div>
         </div>
       </div>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 import VuePaginate from 'vue-paginate'
+import VueRouter from 'vue-router'
 
 Vue.use(VueResource);
 Vue.use(VuePaginate);
+Vue.use(VueRouter);
 
 new Vue({
  el:'#app',
